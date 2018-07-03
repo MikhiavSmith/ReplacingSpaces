bcc3cf7143a1adbba3000c610eea5d9d6f976461
MikhiavSmith <36665285+MikhiavSmith@users.noreply.github.com>
2018-07-03 19:49:28 -0400

    Importing project

---
+++ CharReplacement/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharReplacement
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string r = ReplaceSpaces("I hope this webinar isn't boring.");
+			Console.WriteLine(r);
+			Console.ReadLine();
+		}
+
+		public static string ReplaceSpaces(string s)
+		{
+			return s.Replace(" ", "%20");
+		}
+	}
+}
